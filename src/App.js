d6c26964c05dd8106cf3e9aa3d552146e7c2bdbf
teb92 <147997027+teb92@users.noreply.github.com>
2024-10-27 09:48:38 -0600

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Mensaje from './mensaje.js'
+
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hola Mundo 
+      <Mensaje color='red' message='estamos trabajndo' />
+      <Mensaje color='green' message='trabajdno en' />
+      <Mensaje color='yellow' message='un curso ' />
     </div>
   );
 }
